538b927b9d48489785878bddb58a636f40ccbc61
csl <3079625093@qq.com>
2022-09-22 11:20:25 +0800

    update

--- Targetless_Calibration_of_LiDAR-IMU_System_Based_on_Continuous-time_Batch_Estimation/alg-sim/test/include/test_config.hpp
+++ Targetless_Calibration_of_LiDAR-IMU_System_Based_on_Continuous-time_Batch_Estimation/alg-sim/test/include/test_config.hpp
@@ -10,14 +10,14 @@
 
 TEST(config, Config) {
     using namespace ns_calib;
-    EXPECT_DOUBLE_EQ(Config::LiDAROdometer::NDT::TRANS_EPSILON, 0.01);
+    EXPECT_DOUBLE_EQ(Config::LiDAROdometer::NDT::TRANS_EPSILON, 0.001);
     EXPECT_DOUBLE_EQ(Config::LiDAROdometer::NDT::STEP_SIZE, 0.1);
-    EXPECT_FLOAT_EQ(Config::LiDAROdometer::NDT::RESOLUTION, 1.0f);
-    EXPECT_EQ(Config::LiDAROdometer::NDT::MAX_ITERATIONS, 35);
+    EXPECT_FLOAT_EQ(Config::LiDAROdometer::NDT::RESOLUTION, 2.0f);
+    EXPECT_EQ(Config::LiDAROdometer::NDT::MAX_ITERATIONS, 50);
 
-    EXPECT_FLOAT_EQ(Config::LiDAROdometer::AVGFilter::LEAF_SIZE[0], 0.2f);
-    EXPECT_FLOAT_EQ(Config::LiDAROdometer::AVGFilter::LEAF_SIZE[1], 0.2f);
-    EXPECT_FLOAT_EQ(Config::LiDAROdometer::AVGFilter::LEAF_SIZE[2], 0.2f);
+    EXPECT_FLOAT_EQ(Config::LiDAROdometer::AVGFilter::LEAF_SIZE[0], 0.5f);
+    EXPECT_FLOAT_EQ(Config::LiDAROdometer::AVGFilter::LEAF_SIZE[1], 0.5f);
+    EXPECT_FLOAT_EQ(Config::LiDAROdometer::AVGFilter::LEAF_SIZE[2], 0.5f);
 }
 
 #endif //ALG_SIM_TEST_CONFIG_HPP
